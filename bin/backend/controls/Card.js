57f962fe2eb861dd12cf43a5e3eddad108f6ed54
Jan Wennrich <jan.wennrich@pcsg.de>
2018-12-21 14:45:46 +0100

    feat: Card-control's content & icon can passed on creation + more setters & getters

--- bin/backend/controls/Card.js
+++ bin/backend/controls/Card.js
@@ -20,7 +20,9 @@ define('package/quiqqer/dashboard/bin/backend/controls/Card', [
         Type   : 'package/quiqqer/dashboard/bin/backend/controls/Dashboard',
 
         options: {
+            icon: false,
             title : false,
+            content: false,
             footer: false,
             styles: false
         },
@@ -29,6 +31,7 @@ define('package/quiqqer/dashboard/bin/backend/controls/Card', [
             this.parent(options);
 
             this.$Title   = null;
+            this.$Icon    = null;
             this.$Content = null;
             this.$Footer  = null;
         },
@@ -42,12 +45,16 @@ define('package/quiqqer/dashboard/bin/backend/controls/Card', [
             this.$Elm.addClass('quiqqer-dashboard-card');
             this.$Elm.set({
                 html: '<div class="quiqqer-dashboard-card-container">' +
-                    '   <header class="quiqqer-dashboard-card-title"></header>' +
+                    '   <header class="quiqqer-dashboard-card-header">' +
+                    '       <span class="quiqqer-dashboard-card-icon"></span>'  +
+                    '       <span class="quiqqer-dashboard-card-title"></span>' +
+                    '   </header>' +
                     '   <div class="quiqqer-dashboard-card-content"></div>' +
-                    '   <div class="quiqqer-dashboard-card-footer"></div>' +
+                    '   <div class="quiqqer-dashboard-card-footer"></div>'  +
                     '</div>'
             });
 
+            this.$Icon    = this.$Elm.getElement('.quiqqer-dashboard-card-icon');
             this.$Title   = this.$Elm.getElement('.quiqqer-dashboard-card-title');
             this.$Content = this.$Elm.getElement('.quiqqer-dashboard-card-content');
             this.$Footer  = this.$Elm.getElement('.quiqqer-dashboard-card-footer');
@@ -55,13 +62,23 @@ define('package/quiqqer/dashboard/bin/backend/controls/Card', [
             if (this.getAttribute('title') === false) {
                 this.$Title.setStyle('display', 'none');
             } else if (typeof this.getAttribute('title') === 'string') {
-                this.$Title.set('html', this.getAttribute('title'));
+                this.setTitle(this.getAttribute('title'));
+            }
+
+            if (this.getAttribute('icon')) {
+                this.setIcon(this.getAttribute('icon'));
+            }
+
+            if (this.getAttribute('content') === false) {
+                this.$Content.setStyle('display', 'none');
+            } else if (typeof this.getAttribute('title') === 'string') {
+                this.setContent(this.getAttribute('content'));
             }
 
             if (this.getAttribute('footer') === false) {
                 this.$Footer.setStyle('display', 'none');
             } else if (typeof this.getAttribute('footer') === 'string') {
-                this.$Footer.set('html', this.getAttribute('footer'));
+                this.setFooter(this.getAttribute('footer'));
             }
 
             if (this.getAttribute('styles')) {
@@ -76,10 +93,37 @@ define('package/quiqqer/dashboard/bin/backend/controls/Card', [
         /**
          * Return the title
          *
-         * @return {HTMLHeadElement}
+         * @return {HTMLSpanElement}
          */
         getTitle: function () {
             return this.$Title;
+        },
+
+        /**
+         * Set's the card's title
+         *
+         * @param {string} title
+         */
+        setTitle: function (title) {
+            this.$Title.set('html', title);
+        },
+
+        /**
+         * Returns the icon
+         *
+         * @return {string}
+         */
+        getIcon: function () {
+            return this.$Icon.classList.toString().replace('quiqqer-dashboard-card-icon', '');
+        },
+
+        /**
+         * Set's the card's icon
+         *
+         * @param {string} icon
+         */
+        setIcon: function (icon) {
+            this.$Icon.className = 'quiqqer-dashboard-card-icon ' + icon;
         },
 
         /**
@@ -92,12 +136,30 @@ define('package/quiqqer/dashboard/bin/backend/controls/Card', [
         },
 
         /**
+         * Sets the card's content
+         *
+         * @param {string} content
+         */
+        setContent: function (content) {
+            this.$Content.set('html', content);
+        },
+
+        /**
          * Return the title
          *
          * @return {HTMLDivElement}
          */
         getFooter: function () {
             return this.$Footer;
+        },
+
+        /**
+         * Sets the card's footer
+         *
+         * @param {string} footer
+         */
+        setFooter: function (footer) {
+            this.$Footer.set('html', footer);
         }
     });
 });
