f4c4136a7ea2402c0b60a16eec8bd7d35ece4a63
Henning Leutz <leutz@pcsg.de>
2019-03-13 16:59:18 +0100

    fix: if available handlers are false, this will be considered

--- ajax/backend/getSystemInfo.php
+++ ajax/backend/getSystemInfo.php
@@ -11,12 +11,16 @@ QUI::$Ajax->registerFunction(
     'package_quiqqer_dashboard_ajax_backend_getSystemInfo',
     function () {
         $availableCacheHandlers = QUI\Cache\Manager::getConfig()->getSection('handlers');
-        $activeCacheHandler     = array_search('1', $availableCacheHandlers);
 
-        $activeCacheHandlerTranslated = QUI::getLocale()->get(
-            'quiqqer/system',
-            'quiqqer.settings.cache.handler.' . $activeCacheHandler
-        );
+        if ($availableCacheHandlers) {
+            $activeCacheHandler           = array_search('1', $availableCacheHandlers);
+            $activeCacheHandlerTranslated = QUI::getLocale()->get(
+                'quiqqer/system',
+                'quiqqer.settings.cache.handler.'.$activeCacheHandler
+            );
+        } else {
+            $activeCacheHandlerTranslated = '---';
+        }
 
         return [
             'quiqqerVersion'  => QUI::getPackageManager()->getVersion(),
