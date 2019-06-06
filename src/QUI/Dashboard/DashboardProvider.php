6f9433fa6a04af0d8a086c97dc5d5f804657b86b
Jan Wennrich <jan.wennrich@pcsg.de>
2019-06-06 17:58:50 +0200

    Revert "feat: WIP: adding bookmarks dashboard card (#15)"
    This reverts commit e3a0ffc87c013b4ec933c4f838d5696312296aab.

--- src/QUI/Dashboard/DashboardProvider.php
+++ src/QUI/Dashboard/DashboardProvider.php
@@ -30,8 +30,6 @@ class DashboardProvider implements DashboardProviderInterface
 
             'package/quiqqer/dashboard/bin/backend/controls/cards/Messages',
 
-            'package/quiqqer/dashboard/bin/backend/controls/cards/Bookmarks',
-
             // A whole row of cards. No other cards will be added to this row.
             [
                 'package/quiqqer/dashboard/bin/backend/controls/cards/Stats/Projects',
