cdbb63d887cad5c501f01939a733ffe3245fa82e
Jan Wennrich <jan.wennrich@pcsg.de>
2022-08-26 12:21:29 +0200

    fix: correctly determine if no cache handler is active
    related: quiqqer/dashboard#49

--- ajax/backend/getSystemInfo.php
+++ ajax/backend/getSystemInfo.php
@@ -10,16 +10,20 @@
 QUI::$Ajax->registerFunction(
     'package_quiqqer_dashboard_ajax_backend_getSystemInfo',
     function () {
+        $activeCacheHandler = false;
+        $activeCacheHandlerTranslated = '---';
+
         $availableCacheHandlers = QUI\Cache\Manager::getConfig()->getSection('handlers');
 
         if ($availableCacheHandlers) {
-            $activeCacheHandler           = \array_search('1', $availableCacheHandlers);
+            $activeCacheHandler = \array_search('1', $availableCacheHandlers);
+        }
+
+        if ($activeCacheHandler) {
             $activeCacheHandlerTranslated = QUI::getLocale()->get(
                 'quiqqer/system',
                 'quiqqer.settings.cache.handler.' . $activeCacheHandler
             );
-        } else {
-            $activeCacheHandlerTranslated = '---';
         }
 
         $Packages = QUI::getPackageManager();
